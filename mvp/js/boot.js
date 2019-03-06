0a844f32f5a6d65b1461dd1977763152d463e2f6
Antonio Pedro Ferreira <apsf88@gmail.com>
2019-03-06 11:34:47 -0300

    corrigindo estrutura de pastas

--- mvp/js/boot.js
+++
@@ -1,9 +0,0 @@
-var bootState = {
-    preload: function() {
-        game.load.image('progressBar', 'img/telas/load/progressBar.png')
-    }, 
-
-    create: function() {
-        game.state.start('load')
-    }
-};
